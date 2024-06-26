9e5178d44491f58dcbc16bdc0c1d5d05a465b3fa
Carlos Gonzalez <carlos.gonzalezl@adevinta.com>
2024-06-26 09:01:10 +0200

    persisted plugin

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,19 +1,14 @@
 return {
-    -- Session management. This saves your session in the background,
-    -- keeping track of open buffers, window arrangement, and more.
-    -- You can restore sessions when returning through the dashboard.
     {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        opts = { options = vim.opt.sessionoptions:get() },
-    -- stylua: ignore
-    keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+        "olimorris/persisted.nvim",
+        config = function()
+            vim.o.sessionoptions = "buffers,curdir,folds,globals,tabpages,winpos,winsize"
+            require("persisted").setup({
+                autoload = true,
+            })
+            require("telescope").load_extension("persisted")
+            vim.keymap.set("n", "<leader>ql", ":Telescope persisted<CR>", { noremap = true, silent = true })
+        end,
+        dependencies = { "nvim-telescope/telescope.nvim" },
     },
-    },
-
-    -- library used by other plugins
-    { "nvim-lua/plenary.nvim", lazy = true },
 }
